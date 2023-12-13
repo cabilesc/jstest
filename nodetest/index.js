9c3f2efbfd0052eb9b656fae26302e08d3268122
cabilesc <144515905+cabilesc@users.noreply.github.com>
2023-12-12 17:10:38 -0800

    node

--- nodetest/index.js
+++ nodetest/index.js
@@ -3,7 +3,9 @@ const app = express();
 const { readFile } = require('fs').promises;
 
 app.get('/', async (request,response) => {
-    response.send( await readFile('./home.html', 'utf8') );
+    response.send( await readFile('./public/home.html', 'utf8') );
 });
+
+app.use(express.static("public"))
 
 app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
